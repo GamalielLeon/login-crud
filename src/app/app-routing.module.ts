668272f6ebefc7a58eed4548911b85b1d78b3054
Gamaliel Leon <e-gentrambasaguas@neoris.com>
2020-12-17 22:19:09 -0600

    Add the Router module to the NewPasswordComponent and create some routes in the ap.routing.module file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { NewPasswordFormComponent } from './components/new-password-form/new-password-form.component';
+import { SignUpFormComponent } from './components/sign-up-form/sign-up-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginFormComponent},
+  {path: 'register', component: SignUpFormComponent},
+  {path: 'newPassword', component: NewPasswordFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/new-password-form/new-password-form.component.ts
+++ src/app/components/new-password-form/new-password-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-password-form',
@@ -13,46 +14,32 @@ export class NewPasswordFormComponent implements OnInit {
   // References
   createPasswordForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private router: Router) {
     this.createPasswordForm = formBuilder.group({
       // Required, only admits alphanumerics and must have btw 6 to 12 characters.
       password: ['', [Validators.required, Validators.pattern('([a-zA-Z0-9Ññ_-]){6,12}')]],
       passwordConfirm: ['', [Validators.required, Validators.pattern('([a-zA-Z0-9Ññ_-]){6,12}')]]
     });
    }
-
   ngOnInit(): void { }
 
+  /********** METHODS **********/
   checkSubmit(): void{
-    if (this.getPasswordsMatched()){
-      if (this.createPasswordForm.valid && this.getPasswordsMatched()){
-        console.log('submit event works');
-      }
-    } else{
-      window.alert('Passwords do not match');
-    }
+    if (this.getPasswordsMatched() && this.createPasswordForm.valid) { this.router.navigateByUrl('/login'); }
+    else{ window.alert('Passwords do not match'); }
   }
-
   isFieldInvalid(fieldName: string): boolean{
-    return this.createPasswordForm.controls[fieldName].invalid && this.createPasswordForm.controls[fieldName].touched;
+    const field = this.createPasswordForm.controls[fieldName];
+    return field.invalid && field.touched;
   }
-
-  checkPasswordsMatch(): void{
-    const formControls = this.createPasswordForm.controls;
-    if (formControls.password.value === formControls.passwordConfirm.value){
-      this.setPasswordsMatched(true);
-    } else {
-      this.setPasswordsMatched(false);
-    }
-  }
-
   checkPasswordConfirm(): void{
     const formControls = this.createPasswordForm.controls;
     this.setPasswordsMatched(formControls.password.value === formControls.passwordConfirm.value);
   }
 
+  /********** GETTERS **********/
   getPasswordsMatched = (): boolean => this.passwordsMatched;
 
+  /********** SETTERS **********/
   setPasswordsMatched(passwordsMatched: boolean): void{ this.passwordsMatched = passwordsMatched; }
-
 }

2ba127a50294083217183bf3888ef8461e8b0d7a
Gamaliel Leon <e-gentrambasaguas@neoris.com>
2020-12-18 13:02:58 -0600

    Add new routes to the app.routing file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,17 @@ import { LoginFormComponent } from './components/login-form/login-form.component
 import { NewPasswordFormComponent } from './components/new-password-form/new-password-form.component';
 import { SignUpFormComponent } from './components/sign-up-form/sign-up-form.component';
 import { PopUpWindowComponent } from './components/pop-up-window/pop-up-window.component';
+import { UserFormComponent } from './components/user-form/user-form.component';
+import { UserListComponent } from './components/user-list/user-list.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginFormComponent},
   {path: 'register', component: SignUpFormComponent},
   {path: 'newPassword', component: NewPasswordFormComponent},
   {path: 'popup', component: PopUpWindowComponent},
+  {path: 'detail', component: UserListComponent},
+  {path: 'addUser', component: UserFormComponent},
+  {path: 'editUser', component: UserFormComponent},
   {path: '**', pathMatch: 'full', redirectTo: 'login'}
 ];
 

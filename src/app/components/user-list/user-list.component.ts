893acbfec60919d7943612c1d33c7d84e3ca4813
Gamaliel Leon <e-gentrambasaguas@neoris.com>
2020-12-27 02:36:38 -0600

    Create a simple table in users-list component, in order to show the users brought from the API

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersAPIService } from 'src/app/services/users-api.service';
+import { UserModel } from '../../models/user.model';
 
 @Component({
   selector: 'app-user-list',
@@ -6,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent implements OnInit {
+  usersFromAPI: UserModel[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private usersService: UsersAPIService) {
+    this.usersService.getUsers().subscribe( (users: UserModel[]) => this.usersFromAPI = users );
   }
 
+  ngOnInit(): void { document.body.style.backgroundImage = 'url("")'; }
 }

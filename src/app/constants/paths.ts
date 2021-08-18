fe88269212cb9498a1419332b554784d11c89e21
Gamaliel Leon <gamaliel.leon@neoris.com>
2021-08-18 00:21:34 -0500

    LOGIN path was changed from '' to "login"

--- src/app/constants/paths.ts
+++ src/app/constants/paths.ts
@@ -1,6 +1,6 @@
 export const MAIN: string = 'main';
 export const HOME: string = 'home';
-export const LOGIN: string = '';
+export const LOGIN: string = 'login';
 export const REGISTER: string = 'register';
 export const NEW_PASSWORD: string = 'newPassword';
 export const RECOVER_PASSWORD: string = 'recoverPassword';
